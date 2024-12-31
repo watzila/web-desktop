97f83c41fcc88030a95ec6752cde0d76a64742a2
睿秝-王建淞 <jiansong@chis.com.tw>
2024-12-31 18:01:54 +0800

    加入youtube api播放

---
+++ wwwroot/js/music.js
@@ -0,0 +1,35 @@
+﻿class Music {
+    constructor(obj) {
+        this.volumeBTN = obj.target.getElementById(obj.volumeBTN);
+        this.playBTN = obj.target.getElementById(obj.playBTN);
+        this.pauseBTN = obj.target.getElementById(obj.pauseBTN);
+        this.muteBTN = obj.target.getElementById(obj.muteBTN);
+        this.player;
+        this.musicUrl = [];
+
+        this.init();
+    }
+
+    init() {
+        player = new YT.Player('', {
+            height: '0',
+            width: '0',
+            videoId: this.musicUrl[0], // 替換為你想播放的影片 ID
+            playerVars: {
+                autoplay: 1,
+                controls: 0
+            }
+        });
+
+        console.log(this.player);
+    }
+
+    play() {
+        this.player.videoId = "";
+    }
+
+    addEvent() {
+
+    }
+
+}
